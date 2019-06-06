1a479d965f35993bb142ef897009bf7cec8bd06e
edson.pita <edson.pita.sp@gmail.com>
2019-06-05 22:36:56 -0300

    finish scenario list and edit product closes #5, closes #6

--- features/pages/produtos.rb
+++ features/pages/produtos.rb
@@ -1,23 +1,45 @@
+# frozen_string_literal: true
+
 class Produtos
-    include RSpec::Mocks::ExampleMethods::ExpectHost
-    include RSpec::Matchers
-    include Capybara::DSL
+  include RSpec::Mocks::ExampleMethods::ExpectHost
+  include RSpec::Matchers
+  include Capybara::DSL
 
-    def page_produto
-       click_link('Produtos')
-       click_link('Novo Produto')
-    end
+  def page_produto
+    click_link('Produtos')
+    click_link('Novo Produto')
+  end
 
-    def preencher_dados(campo,valor)
-        fill_in("product[#{campo}]", :with => valor)
-    end
-    def clicar_create
-        click_button('Create Product')
-    end
+  def page_listar_produto
+    click_link('Produtos')
+    click_link('Listar Produtos')
+  end
 
-    def select_from(id, value)
-      page.find_by_id(id).find("option[value=#{value}]").select_option
-    end
+  def preencher_dados(campo, valor)
+    fill_in("product[#{campo}]", with: valor)
+  end
 
+  def clicar_criar_produto
+    click_button('Create Product')
+  end
 
+  def select_from(id, value)
+    page.find_by_id(id).find("option[value=#{value}]").select_option
+  end
+
+  def link_proxima_pagina
+    click_link('Próximo')
+  end
+
+  def clicar_produto
+    find('body > div > table > tbody > tr:nth-child(2)').click
+end
+
+  def clicar_atualizar_produto
+    click_button('Update Product')
+  end
+
+  def count_by(_value)
+    page.all('tr.hover-cursor', visible: true, maximum: 10, minimum: 1)
+  end
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -7,12 +7,29 @@ require 'capybara/dsl'
 require 'selenium-webdriver'
 require 'securerandom'
 
-Capybara.register_driver :chrome do |app|
-	Capybara::Selenium::Driver.new(app, :browser => :chrome)
+case ENV['BROWSER']
+	when 'pg'
+		# using poltergeist
+		DRIVER = :poltergeist
+		Capybara.register_driver :poltergeist_debug do |app|
+		  Capybara::Poltergeist::Driver.new(app, phantomjs_options: ['--ignore-ssl-errors=yes'])
+		end
+	when 'ff'
+		# using firefox
+		DRIVER = :firefox
+		Capybara.register_driver :firefox do |app|
+			Capybara::Selenium::Driver.new(app, :browser => :firefox)
+		end
+	else
+		# using chrome
+		DRIVER = :chrome
+		Capybara.register_driver :chrome do |app|
+			Capybara::Selenium::Driver.new(app, :browser => :chrome)
+		end
 end
 
 Capybara.configure do |config|
-	config.default_driver = :chrome
+	config.default_driver = DRIVER
 	config.app_host = 'http://qainterview.qa.cloud.bionexo.com.br'
   config.default_max_wait_time = 30
 
